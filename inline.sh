496841e3e7e7b63aeed5c8449d65f7960b99fc4a
Armin Braun <armin.b@icanlocalize.com>
2015-12-19 19:24:53 +0100

    added runs including xdebug

--- inline.sh
+++ inline.sh
@@ -4,4 +4,6 @@ for PHP_VERSION in "5.4" "5.5" "5.6" "7"
 do
 	echo -e "\nPHP ${PHP_VERSION}\n"
 	docker run -t --rm -v "$PWD":/src php:${PHP_VERSION}-cli php /src/inline.php
+	echo -e "\nPHP ${PHP_VERSION} + XDebug\n"
+	docker run -t --rm -v "$PWD":/src originalbrownbear/php:${PHP_VERSION}-cli-xdebug php /src/inline.php
 done
